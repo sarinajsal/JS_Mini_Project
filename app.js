1862a3f181bc8533ee1e7d85f8f714253932da2b
Sarina Salamon <salamon.sarina@gmail.com>
2022-03-09 22:08:23 +0000

    stuff

---
+++ app.js
@@ -0,0 +1,8 @@
+//on hover, user can see cross (alternate)
+//squares need to have onlclick event 
+//onclick, an x needs to be placed on that specific cell
+//a function to check whether 3 in a row has been achieved - 
+//if win or draw, stop game and show restart button
+//if restart button clicked, clear the grid and start game again
+//if the game is not won/drawn, alternate player then plays 
+
