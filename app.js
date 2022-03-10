a4c3d793cff747eb10a340ec3d2a659df9ecf63c
Adib Baig <adibzbaig@gmail.com>
2022-03-10 17:10:33 +0000

    added winningConditions and method

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let playerName = 'Player 1'
 const cellElements = document.querySelectorAll('[data-cell]');
 const resultMessage = document.querySelector("#result-message");
 
-const handleClick = (clickedCellEvent) => {
+const handleClickPlayer = (clickedCellEvent) => {
     const clickedCell = clickedCellEvent.target;
     currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
     clickedCell.innerText = currentPlayer;
@@ -13,7 +13,7 @@ const handleClick = (clickedCellEvent) => {
 }
 
 cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, {once : true})
+    cell.addEventListener('click', handleClickPlayer, {once : true})
 })
 
 // reset the page when restart button clicked
@@ -23,12 +23,34 @@ restartButton.addEventListener("click", restartGame)
 function restartGame () {
     cellElements.forEach(cell => {
         resultMessage.innerText = "";
-        cell.removeEventListener("click", handleClick, {once : true})
+        cell.removeEventListener("click", handleClickPlayer, {once : true})
         cell.innerText = "";
-        cell.addEventListener('click', handleClick, {once : true})
+        cell.addEventListener('click', handleClickPlayer, {once : true})
     })
 }
 
 // Logic for win and draw
+const winningConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
+function winningMethod(currentPlayer) {
+    return winningConditions.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentPlayer)
+        })
+    })
+}
 
+const handleClickWin = () => {
+    if (winningMethod(currentPlayer)) {
+        
+    }
+}
