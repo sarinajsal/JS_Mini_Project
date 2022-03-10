7055e2e12769511fd57d4b780c48418d5702c43f
Darshil Dholakia <darshildholakia1@gmail.com>
2022-03-10 13:45:08 +0000

    added functionality to reset button and result message

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
+let currentPlayer = 'x';
+let playerName = 'Player 1'
+const cellElements = document.querySelectorAll('[data-cell]');
+const resultMessage = document.querySelector("#result-message");
 
-        let currentPlayer = 'x';
+const handleClick = (clickedCellEvent) => {
+    const clickedCell = clickedCellEvent.target;
+    currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
+    clickedCell.innerText = currentPlayer;
 
-        const handleClick = (clickedCellEvent) => {
-            const clickedCell = clickedCellEvent.target;
-            currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
-            clickedCell.innerText = currentPlayer;
-            console.log("clicked")
-        }
+    playerName = currentPlayer === 'x' ? 'Player 1' : 'Player 2';
+    resultMessage.innerText = `${playerName}, it's your turn!`
+}
 
-        const cellElements = document.querySelectorAll('[data-cell]');
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, {once : true})
+})
 
-        cellElements.forEach(cell => {
-            cell.addEventListener('click', handleClick, {once : true})
-        })
+// reset the page when restart button clicked
+const restartButton = document.querySelector("#restart-button");
+restartButton.addEventListener("click", () => {
+    cellElements.forEach(cell => {
+        resultMessage.innerText = "";
+        cell.removeEventListener("click", handleClick, {once : true})
+        cell.innerText = "";
+        cell.addEventListener('click', handleClick, {once : true})
+    })
+})
+
+// Logic for win and draw
+
 
